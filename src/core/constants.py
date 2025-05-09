49563f29978f913dcdb10c43d94b915b39845b20
Bemake <271374667@qq.com>
2025-05-09 15:47:12 +0800

    feature: 实现了命令行可交互的程序

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,4 +1,5 @@
 from typing import Final
+import textwrap
 
 COMMON_IMAGE_SUFFIXES: Final[tuple[str, ...]] = (
     ".jpg",
@@ -8,3 +9,14 @@ COMMON_IMAGE_SUFFIXES: Final[tuple[str, ...]] = (
     ".bmp",
     ".webp",
 )
+
+ASCII_LOGO = textwrap.dedent("""
+ _____                     _____           _     
+|_   _|                   |_   _|         | |    
+  | | _ __ ___   __ _       | | ___   ___ | |___ 
+  | || '_ ` _ \ / _` |      | |/ _ \ / _ \| / __|
+ _| || | | | | | (_| |      | | (_) | (_) | \__ \\
+ \___/_| |_| |_|\__, |      \_/\___/ \___/|_|___/
+                 __/ |                           
+                |___/                            
+    """)
